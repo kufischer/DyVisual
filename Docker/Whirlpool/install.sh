04c2b8513b647397dcbbe03d85b2e8ca220098a6
root <root@employee-static-32-26.sb.dfki.de>
2016-07-22 17:17:52 +0200

    Fixed yet another path problem ...

--- Docker/Whirlpool/install.sh
+++ Docker/Whirlpool/install.sh
@@ -7,9 +7,9 @@ WPHOME=/root/DyVisual/Whirlpool/WhirlpoolFiVES
 echo ${FIVESHOME}
 echo ${WEBAPPS}
 
-cp -r ${WPHOME}/DeviationMaps ${FIVESHOME}/Plugins/DeviationMaps
+cp -r ${WPHOME}/DeviationMaps ${FIVESHOME}/Plugins/
 
-cp -r ${WPHOME}/WebClientWp ${WEBAPPS}
+cp -r ${WPHOME}/WebClientWp ${WEBAPPS}/
 
 cp -f ${WPHOME}/FIVES.sln ${FIVESHOME}
 
