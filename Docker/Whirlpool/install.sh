5ac0b3d206e21f14f9c49479f2771536ecaf2f02
root <root@employee-static-32-26.sb.dfki.de>
2016-07-25 11:35:55 +0200

    Ok, falsche Reihenfolge im Docker file

--- Docker/Whirlpool/install.sh
+++ Docker/Whirlpool/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FIVESHOME=/root/fives
-WEBAPPS=/var/www
+WEBAPPS=/var/www/
 WPHOME=/root/DyVisual/Whirlpool/WhirlpoolFiVES
 
 echo ${FIVESHOME}
@@ -9,9 +9,7 @@ echo ${WEBAPPS}
 
 cp -r ${WPHOME}/DeviationMaps ${FIVESHOME}/Plugins/
 
-mkdir ${WEBAPPS}/WebClientWp
-
-cp -r ${WPHOME}/WebClientWp ${WEBAPPS}/WebClientWp
+cp -r ${WPHOME}/WebClientWp ${WEBAPPS}
 
 cp -f ${WPHOME}/FIVES.sln ${FIVESHOME}
 
